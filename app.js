02909479ed1d59ebef810edb7e12404ffe358b3f
Guilherme Coelho <guifernandes.dev@gmail.com>
2025-06-17 21:40:54 -0300

    Codificando o botão reiniciar

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+let botaoReiniciar = document.getElementById("btn-reiniciar");
+let botaoSortear = document.getElementById("btn-sortear")
+
 function sortear(){
     let quantidade = parseInt(document.getElementById("quantidade").value);
     let de = parseInt(document.getElementById("de").value);
     let ate = parseInt(document.getElementById("ate").value);
-
     let sorteados = [];
     let numero;
 
@@ -17,15 +19,32 @@ function sortear(){
 
     let resultado = document.getElementById("resultado");
     sorteados.length == 1 ? resultado.innerHTML = `<label class="texto__paragrafo">Número sorteado: ${sorteados} </label>` : resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados} </label>`;
+
+    alterarStatusBotao();
+}
+
+function alterarStatusBotao(){
+    if(botaoReiniciar.classList.contains("container__botao-desabilitado")){
+        botaoReiniciar.classList.remove("container__botao-desabilitado");
+        botaoReiniciar.classList.add("container__botao");
+        botaoSortear.classList.remove("container__botao");
+        botaoSortear.classList.add("container__botao-desabilitado");
+    }else{
+        botaoReiniciar.classList.remove("container__botao");
+        botaoReiniciar.classList.add("container__botao-desabilitado");
+        botaoSortear.classList.remove("container__botao-desabilitado");
+        botaoSortear.classList.add("container__botao");
+    }
 }
 
 function obterNumeroAleatorio(minimo, maximo){
     return Math.floor(Math.random() * (maximo - minimo + 1) + minimo);
 }
 
-
-
-
 function reiniciar(){
-
+    document.getElementById("quantidade").value = "";
+    document.getElementById("de").value = "";
+    document.getElementById("ate").value = "";
+    document.getElementById("resultado").innerHTML = `<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>`;
+    alterarStatusBotao();
 }
